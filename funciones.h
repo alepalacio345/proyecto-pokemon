8e664766e68bbea4cadad16091781510e302ac80
alepalacio345 <edicionlara@gmail.com>
2026-09-12 12:31:13 -0400

    se crearon el tipo de dato del ejemplar pokemon y el entrenador

--- funciones.h
+++ funciones.h
@@ -1,5 +1,7 @@
 #ifndef funciones
 #define funciones
+#include <stdbool.h>
+#define MAX_POKEMON 6
 
 typedef struct{
      char ID[6];
@@ -11,6 +13,40 @@ typedef struct{
      int defensa_b;
      int velocidad_b;
 }especies_pokemons;
+
+
+typedef struct {
+    int id_ejemplar;                  // EXIGIDO: Para diferenciar dos Pokémon iguales
+    especies_pokemons* datos_especie; // EXIGIDO: El puntero a la especie
+    char nombre[20];                 
+    int nivel;                        
+    
+    // Estadísticas generadas
+    int hp_actual;                    
+    int hp_maximo;                    // RECOMENDADO: Tope de vida para curarlo
+    int ataque;                      
+    int defensa;                      
+    int velocidad;                    
+    
+    int experiencia;                  
+    bool estado;                     
+} EjemplarPokemon;
+
+
+typedef struct {
+    int id_entrenador;
+    char nombre[30];
+    
+    // El arreglo que intuiste, guardando a los ejemplares del equipo
+    EjemplarPokemon equipo[MAX_POKEMON]; 
+    int cantidad_pokemon; // Útil para saber cuántos Pokémon tiene asignados realmente
+    
+    // Estadísticas para la tabla de posiciones de la Fase de Grupos
+    int victorias;
+    int empates;
+    int derrotas;
+    int puntuacion;
+} Entrenador;
 
 void llenar_vector_pokemon(especies_pokemons vector[], int *i);
 void imprimir_vector_pokemon(especies_pokemons vector[], int cantidad);
--- pokemon.c
+++ pokemon.c
@@ -7,7 +7,7 @@ int main(){
 
     llenar_vector_pokemon(vector,&cantidad);
     //imprimir_vector_pokemon(vector,i);
-    buscar_pokemon_por_id(vector,0001,cantidad);
+    buscar_pokemon_por_id(vector,001,cantidad);
 
     buscar_pokemon_nombre(vector,"Mew",cantidad);
 
